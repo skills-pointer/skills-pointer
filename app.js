c5e2f5a4742042404fece1c440c38382be7e4f8a
marc <m.k.quast@gmail.com>
2018-05-10 10:37:28 +0200

    Introduced Service object

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-let sample = {
+class Service {
+  constructor(database) {
+    this.db = new Database(database);
+  }
+
+  findPersonsWithSkill(skill) {
+    let result = {};
+    recommendations
+            .filter(r => r.skill === skill)
+            .forEach(r => {
+              if (result[r.recommendee]) {
+                ++result[r.recommendee];
+              } else {
+                result[r.recommendee] = 1;
+              }
+            });
+    return result;
+  }
+  
+  reset() {
+    for (let collection in SAMPLE_DATA) {
+      this.db.set(collection, sample[collection]);
+    }
+  }
+}
+
+
+
+const db = new Database('skillspointer');
+
+let persons = db.get('persons');
+let recommendations = db.get('recommendations');
+let user = persons[3];
+
+
+
+// ---
+
+const SAMPLE_DATA = {
   persons: [
     {
       "id": 1,
@@ -26,11 +64,3 @@ let sample = {
     {recommender: 4, recommendee: 1, skill: "C#"},
   ]
 };
-
-const db = new Database('skillspointer');
-
-function reset() {
-  for (let collection in sample) {
-    db.set(collection, sample[collection]);
-  }
-}
